7910114935a4aab31a3a436facc4a4d6fe75d04e
Gaurav Gupta <gauravgupta742002@gmail.com>
2023-07-29 02:03:10 +0530

    vercel deployment 2

--- client/src/Actions/BASE_URL.js
+++ client/src/Actions/BASE_URL.js
@@ -1,3 +1,5 @@
 // BASE_URLs
 
-export const BASE_URL = "http://localhost:4000";
+// export const BASE_URL = "http://localhost:4000";
+
+export const BASE_URL = "https://e-commerce-app-lovat-rho.vercel.app/";
